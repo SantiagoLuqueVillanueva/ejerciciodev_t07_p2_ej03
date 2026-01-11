a4571f7098965004c2576c821b17c46c0f9b1b07
Santiago Luque Villanueva <santiagoluquevillanueva@MacBook-Air-de-Santiago.local>
2026-01-11 14:37:10 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int [][] num = new int [4][5];
+        int [] sumaColumna = new int [5];
+        int sumaTotalGeneral = 0;
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("Fila " + i + ", Columna " + j + ": ");
+                num[i][j] = (int)(Math.random() * 900) + 100;
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 4; i++) {
+            int sumaFila = 0;
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%5d", num[i][j]);
+                sumaFila += num [i][j];
+                sumaColumna[j] += num [i][j];
+            }
+            System.out.printf(" | SUMA:%5d", sumaFila);
+            System.out.println();
+        }
+
+        System.out.println("---------------------------------------------");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.printf("%5d", sumaColumna[i]);
+            sumaTotalGeneral += sumaColumna[i]; 
+        }
+
+        System.out.printf(" | TOTAL:%5d", sumaTotalGeneral);
+        System.out.println();
     }
 }
